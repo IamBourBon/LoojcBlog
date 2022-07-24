f8965613107f41e3c4fc9d7d5f1653c128b294b6
IamBourBon <tuanminh4777@gmail.com>
2022-07-25 01:42:34 +0700

    last1

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -2,9 +2,9 @@
 class database{
 
 	//DB Params
-	private $dns = "mysql:host=localhost;dbname=loojcblog";
-	private $username = "root";
-	private $password = "";
+	private $dns = "mysql:host=bkjwkzrx3hols4hf37li-mysql.services.clever-cloud.com;dbname=bkjwkzrx3hols4hf37li";
+	private $username = "uhbvrnllxdbu8m7i";
+	private $password = "o3J1LyWxMUtMxNevG3gA";
 	private $conn;
 
 	//DB Connect
